14a07e97d1bba89b83d24e8cf4e3e261d916ac70
CozmoFPS <taingo2005@gmail.com>
2026-04-16 06:13:31 -0700

    Delete website directory

--- website/ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apps: [{
-    name: 'csgocc',
-    script: 'index.js',
-    out_file: '/home/csgocomp/logs/out.log',
-    error_file: '/home/csgocomp/logs/error.log',
-    log_date_format: 'YYYY-MM-DD HH:mm:ss'
-  }]
-}
--- website/src/configs/admin.config.js
+++
@@ -1,7 +0,0 @@
-const configAdmin = {
-    adminSteamIds: [
-        "76561198169903401"
-    ]
-}
-
-module.exports = configAdmin
