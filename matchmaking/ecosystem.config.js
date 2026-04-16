d12d487643eb6f49ca772ce0ddc7ca483e551d8d
CozmoFPS <taingo2005@gmail.com>
2026-04-16 06:13:20 -0700

    Delete matchmaking directory

--- matchmaking/ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apps: [{
-    name: 'csgocc_mm',
-    script: 'index.js',
-    out_file: '/home/csgocomp/logs/out_mm.log',
-    error_file: '/home/csgocomp/logs/error_mm.log',
-    log_date_format: 'YYYY-MM-DD HH:mm:ss'
-  }]
-}
